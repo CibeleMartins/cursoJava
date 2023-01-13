a3271a5d7adfa68f7f5e93f20d5ef6b34723f865
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-13 13:35:37 -0300

    Implementou a área lateral de um cilindro - variáveis

--- exercícios/src/exercícios/PrimeiroPrograma.java
+++ exercícios/src/exercícios/PrimeiroPrograma.java
@@ -1,5 +1,23 @@
 package exercícios;
 
-public class Teste {
-
+public class PrimeiroPrograma {
+	
+		
+		@SuppressWarnings("unused")
+		public static void main(String[] args) {
+			
+			System.out.println("------Primeiro programa----------");
+				
+			int raio = 6;
+			int altura = 12;
+			double pi = 3.14;
+			
+			double areaLateral = raio * altura;
+			
+			areaLateral = (2*pi) * areaLateral;
+			
+			System.out.println(areaLateral + " cm2");
+					
+			
+		}
 }
