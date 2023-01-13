bee54918d62615184f651f0a9ae3f3a38085690c
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-13 16:07:52 -0300

    Inseriru comentários no TipoString

--- exercícios/src/exercíciosFundamentos/TipoString.java
+++ exercícios/src/exercíciosFundamentos/TipoString.java
@@ -8,14 +8,24 @@ public class TipoString {
 		String texto = "Tipo String";
 		
 		System.out.println(texto.charAt(3));
+//		retorna a letra que ocupa o index indicado no parametro
 		System.out.println(texto.toLowerCase().startsWith("t"));
+//		verifica se a string começa c/ o que foi indicado no parametro
 		System.out.println(texto.toLowerCase().endsWith("t"));
+//		verifica se a string termina c/ o que foi indicado no parametro
 		System.out.println(texto.toLowerCase().endsWith("g"));
+//		verifica se a string termina c/ o que foi indicado no parametro
 		System.out.println(texto.toLowerCase().length());
+//		retorna o tamanho da string
 		System.out.println(texto.toLowerCase().equals("Tipo String"));
+//		verifica a igualdade da string em relacao a que foi passada no parametro
 		System.out.println(texto.toLowerCase().equalsIgnoreCase("Tipo String"));
+//		verifica a igualdade da string em relacao a que foi passada no parametro 
+//		ignorando letras maiusculas ou minusculas
 		System.out.println(texto.toLowerCase().contains("tring"));
+//		verifica se na string contem o que foi passado no parametro
 		System.out.println(texto.toLowerCase().substring(5, 10));
+//      "pega"/acessa a string entre um indice inicial e final
 		
 		int idade = 10;
 		
