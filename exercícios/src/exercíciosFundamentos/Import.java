ab916743c9b9b7a51410827ac5529644d817d517
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-13 15:25:42 -0300

    Notação ponto e import

---
+++ exercícios/src/exercíciosFundamentos/Import.java
@@ -0,0 +1,20 @@
+package exercíciosFundamentos;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		
+		System.out.println(d);
+				
+		JButton botao = new JButton();
+		//		um botao para interface gráfica
+		
+	}
+
+}
---
+++ exercícios/src/exercíciosFundamentos/NotacaoPonto.java
@@ -0,0 +1,14 @@
+package exercíciosFundamentos;
+
+public class NotacaoPonto {
+		
+	public static void main(String[] args) {
+		 
+		String s = "Notacao ponto";
+		
+		s = s.replace('o', '0').replace('t', '7').toUpperCase();
+		
+		System.out.println(s);
+		
+	}
+}
